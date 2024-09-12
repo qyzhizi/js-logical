0f89f7e1030197f089d28a14252761b5ce82d4c7
qyzhizi <2830942138@qq.com>
2024-09-12 22:04:36 +0800

    update

---
+++ 2024-09-12-webpack-react-app/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  // mode: 'development',
+  // mode: 'production', // 使用生产模式
+  entry: './src/index.js', // 入口文件
+  output: {
+    // filename: 'bundle.js',
+    filename: '[name].[contenthash].js', // 使用内容哈希生成文件名
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/', // 使得应用能够正确地加载资源
+  },
+  module: {
+    //  rules: [
+    //    {
+    //      test: /\.js$/,
+    //      exclude: /node_modules/,
+    //      use: 'babel-loader', // 使用 raw-loader 作为普通加载器
+    //    },
+    //  ],
+   rules: [
+     {
+       test: /\.js$/,
+       loader: 'esbuild-loader',
+       options: {
+         // loader: 'jsx', // Remove this if you don't use JSX
+         target: 'es2015'  
+       }
+     }
+   ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // 在每次构建前清理 build 文件夹
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // HTML 模板
+    }),
+  ],
+  optimization: {
+    minimize: true, // 启用代码压缩
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true, // 去掉 console.log
+          },
+        },
+      }),
+    ],
+    splitChunks: {
+      chunks: 'all', // 代码分割
+    },
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'), // 指定静态资源目录
+    },
+    compress: true, // 启用 gzip 压缩
+    port: 9000, // 指定端口
+    hot: true, // 启用热模块替换
+  },
+  // devtool: 'source-map', // 生成源映射文件
+};
