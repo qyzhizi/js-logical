9c9b6532405434fb5a597306d7d2c109c5d13fe1
qyzhizi <2830942138@qq.com>
2025-01-09 23:06:40 +0800

    update

--- 2024-09-26-vite-react-tailwind-chatgpt-demo/functions/api/azurechatgpt.js
+++ 2024-09-26-vite-react-tailwind-chatgpt-demo/functions/api/azurechatgpt.js
@@ -5,34 +5,30 @@ export async function onRequestPost(context) {
         // const text = await request.text(); // Correctly read request body
         // Check if the content type is JSON
         const contentType = request.headers.get("content-type");
-        let text;
-        if (contentType && contentType.includes("application/json")) {
+        let messages = null;
+        if (contentType?.includes("application/json")) {
             // Parse the JSON body
             const body = await request.json();
-            text = body.text; // Access the 'text' field
-        }else {
+            messages = body.messages;
+        } else {
             return new Response('Unsupported Content-Type', { status: 400 });
         }
-
-        const requestData = {
-            messages: [{ role: 'user', content: text }],
-            // messages: [{ role: 'user', content: 'hello' }],
-        };
+        
+        const requestData = { messages: messages };
 
         const azure_openai_key = env.AZURE_OPENAI_API_KEY;
-        const azure_openai_endpoint = env.AZURE_OPENAI_ENDPOINT
+        const azure_openai_url = env.AZURE_OPENAI_URL;
+
         const requestOptions = {
             method: 'POST',
             headers: {
                 'api-key': `${azure_openai_key}`,
                 "Content-Type": "application/json",
-                // "User-Agent":"PostmanRuntime/7.40.0"
             },
             body: JSON.stringify(requestData),
             // body: requestData,
         };
-        const url = azure_openai_endpoint + '/openai/deployments/gpt-35-turbo/chat/completions?api-version=2024-02-01'
-        const chatres = await fetch(url, requestOptions);
+        const chatres = await fetch(azure_openai_url, requestOptions);
         
         if (!chatres.ok) {
             // Handle non-200 responses
