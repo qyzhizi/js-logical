306ab65d18cbbb353b2c055b88fce2424541e929
qyzhizi <2830942138@qq.com>
2025-03-31 18:40:05 +0800

    remove secrets

--- 2025-03-31-ts-github-accesstoken-ts/index.ts
+++ 2025-03-31-ts-github-accesstoken-ts/index.ts
@@ -1,7 +1,7 @@
 import { fetchGitHubUserInfo } from './test1';
 
 async function getUserInfo() {
-    const res = await fetchGitHubUserInfo('ghu_Uk12q1YreQFntg5TEVS5HUPjGhvLLf0DI2Pi');
+    const res = await fetchGitHubUserInfo('xx_access_token_xx');
     console.log(res);
 }
 
--- 2025-03-31-ts-github-auth/test2.js
+++ 2025-03-31-ts-github-auth/test2.js
@@ -1,3 +1,3 @@
 import {fetchGitHubUserInfo} from './test1.js'
-const res = await fetchGitHubUserInfo('ghu_Uk12q1YreQFntg5TEVS5HUPjGhvLLf0DI2Pi')
+const res = await fetchGitHubUserInfo('access_token_xxxx')
 console.log(res)
